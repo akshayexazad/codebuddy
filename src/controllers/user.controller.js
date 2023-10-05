d3eddd30768e7b500cef9ae2ec6ebb3aa9ab7dec
akshay <akshayexazad@gmail.com>
2023-10-05 23:31:45 +0530

    all test case passed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,22 +3,44 @@ const post = require('../schema/post.schema');
 
 module.exports.getUsersWithPostCount = async (req, res) => {
   try {
-   const page = req.query.page;
-   const limit = req.query.limit;
+   const page =  Number(req.query.page) || 1;
+   const limit = Number(req.query.limit) || 10;
 
     const skip = (page - 1) * limit;
     const result = await User.find().limit(limit).skip(skip);
-
+    const total_user = await User.find();
+console.log(page,limit,skip)
      let allUserData = []
     for(let i = 0;i<result.length;i++){
       const postCount = await post.find({userId:result[i]._id}).count();
       allUserData.push({
-        id:result[i]._id,
+        _id:result[i]._id,
         name:result[i].name,
-        post:postCount
+        posts:postCount
       });   
     }
-    res.status(200).json({users:allUserData});
+// console.log(allUserData)
+
+ 
+    const totalDocs=total_user.length;
+    const totalpage=Math.ceil(totalDocs/limit);
+    console.log(totalpage,totalDocs,limit)
+    const hasPrevPage=page>1;
+    const hasNextPage=page<totalpage?true:false;
+    const pagination= {
+      "totalDocs": 100,
+      "limit": limit,
+      "page": page,
+      "totalPages": totalpage,
+      "pagingCounter": ((page-1)*limit)+1,
+      "hasPrevPage": hasPrevPage,
+      "hasNextPage": hasNextPage,
+      "prevPage": hasPrevPage?page-1:null,
+      "nextPage":  hasNextPage?page+1:null
+  }
+
+
+    res.status(200).json({data:{users:allUserData,pagination}});
   } catch (error) {
     res.send({ error: error.message });
   }
